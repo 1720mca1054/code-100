7bb25707b8df13fd10a5f80db613aa97f149a316
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-12-12 22:59:49 +0530

    Update ClassLoader.java

--- ClassLoader.java
+++ ClassLoader.java
@@ -5,7 +5,7 @@ public class ClassLoader
         // Let's print the classloader name of current class.   
         //Application/System classloader will load this class  
         Class c=ClassLoader.class;  
-        System.out.println(c.getClassLoader());  
+        System.out.println(c.getClassLoader());    
         //If we print the classloader name of String, it will print null because it is an  
         //in-built class which is found in rt.jar, so it is loaded by Bootstrap classloader  
         System.out.println(String.class.getClassLoader());  
